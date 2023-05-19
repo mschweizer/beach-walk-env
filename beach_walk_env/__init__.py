010c1f2d89bd44e0d9a157eb9287273fbddb2506
marvin.schweizer <marvin.schweizer@kit.edu>
2023-05-19 12:24:31 +0200

    Add default wrappers

--- beach_walk_env/__init__.py
+++ beach_walk_env/__init__.py
@@ -1,7 +1,23 @@
 from gymnasium import register
+from gymnasium.envs.registration import WrapperSpec
+from minigrid.wrappers import FullyObsWrapper
+
 from beach_walk_env.beach_walk import *
+from beach_walk_env.observation_wrapper import CustomObsWrapper
 
 register(
     id="BeachWalk-v0",
     entry_point="beach_walk_env:BeachWalkEnv",
+    additional_wrappers=(
+        WrapperSpec(
+            name=FullyObsWrapper.class_name(),
+            entry_point=f"beach_walk_env:FullyObsWrapper",
+            kwargs={}
+        ),
+        WrapperSpec(
+            name=CustomObsWrapper.class_name(),
+            entry_point=f"beach_walk_env:CustomObsWrapper",
+            kwargs={}
+        )
+    )
 )
